c88bdf1811d11a3f6bc7d20811f388d30fd07335
Qamar Ali <qamarali205@gmail.com>
2023-08-03 11:28:10 +0530

    Time: 1 ms (99.78%), Space: 39.7 MB (70.81%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,18 @@
 class Solution {
     public int mySqrt(int x) {
-         return (int)Math.sqrt(x);        
+        long low = 0;
+        long high = x;
+        while (low <= high) {
+            long mid = (low + high) / 2;
+            if (mid * mid == x) {
+                return (int) mid;
+            }
+            if (mid * mid < x) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return (int) high;      
     }
 }

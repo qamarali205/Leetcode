9d07382af7f390149500af5d7570cfa92ad87b44
Qamar Ali <qamarali205@gmail.com>
2023-08-20 20:57:22 +0530

    Time: 78 ms (35.25%), Space: 42 MB (23.92%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.js
+++ 1013-fibonacci-number/1013-fibonacci-number.js
@@ -3,10 +3,12 @@
  * @return {number}
  */
 var fib = function(n) {
-    const arr=[0,1];
-    for(let i=2; i<=n; i++){
-        arr.push(arr[i-1]+arr[i-2]);
-    }
-    return arr[n];
+    // const arr=[0,1];
+    // for(let i=2; i<=n; i++){
+    //     arr.push(arr[i-1]+arr[i-2]);
+    // }
+    // return arr[n];
+    if(n<=1) return n;
+    return fib(n-1)+fib(n-2);
     
 };
